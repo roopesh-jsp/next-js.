e72a513d8dba15921bfffbb2f77e7010cb3dafa2
roopesh-jsp <rupzkumar5@gmail.com>
2025-08-07 13:31:24 +0530

    initilized

---
+++ authjs/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto my-2">
+      <h1 className="text-3xl text-center font-extrabold my-10 ">Home</h1>
+    </div>
+  );
+}
